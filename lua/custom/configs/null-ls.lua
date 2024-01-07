7cc8ef40111196533d7083e0c59bcfb3aca00abf
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-01-07 15:43:24 +0100

    chore: remove comments

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -3,14 +3,10 @@ local null_ls = require "null-ls"
 local b = null_ls.builtins
 
 local sources = {
-
   -- webdev stuff
-  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  -- b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
   b.formatting.prettier,
   -- Lua
   b.formatting.stylua,
-
   -- cpp
   b.formatting.clang_format,
 }
@@ -27,9 +23,7 @@ require("null-ls").setup {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-          vim.lsp.buf.format({ async = false})
+          vim.lsp.buf.format { async = false }
         end,
       })
     end
