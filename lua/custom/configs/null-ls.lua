4cce8d33f2800838c3ae352a627eadaef14d27ce
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-01-07 15:41:12 +0100

    chore: add prettier format on save

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -5,9 +5,9 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
-
+  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  -- b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier,
   -- Lua
   b.formatting.stylua,
 
@@ -15,7 +15,23 @@ local sources = {
   b.formatting.clang_format,
 }
 
-null_ls.setup {
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+require("null-ls").setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+          vim.lsp.buf.format({ async = false})
+        end,
+      })
+    end
+  end,
 }
