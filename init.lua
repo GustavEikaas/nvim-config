6c78b15939199cc17cb1206f23d84dc0a33719ac
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2024-01-30 07:48:04 +0100

    prefer powershell

--- init.lua
+++ init.lua
@@ -2,6 +2,20 @@ require "core"
 require "remap"
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+vim.opt.shell = "powershell"
+
 if custom_init_path then
   dofile(custom_init_path)
 end
