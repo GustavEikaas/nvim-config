ad108c7532c1dec5095de47e9730e146d0358f27
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2024-01-30 18:06:57 +0100

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -284,14 +284,28 @@ local default_plugins = {
       end
     end,
   },
-  {
+   {
     "aznhe21/actions-preview.nvim",
-    keys = { "<leader>", "ca" },
     config = function()
-      vim.keymap.set({ "v", "n" }, "ca", require("actions-preview").code_actions)
+      require("actions-preview").setup {
+        telescope = {
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = "top",
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines)
+              return max_lines - 15
+            end,
+          },
+        },
+      }
     end,
-    lazy = true,
+    lazy = false,
   },
+
   -- Only load whichkey after all the gui
   {
     "folke/which-key.nvim",
