cbdee3e95ade5fa7b542c885fab89a26327ec832
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-24 20:08:06 +0100

    add non functioning dap

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,8 +4,9 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier
+      null_ls.builtins.formatting.prettier,
     })
+    null_ls.register { name = "c#", filtetypes = { "cs" }, sources = { null_ls.builtins.formatting.csharpier } }
     vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
   end
 }
