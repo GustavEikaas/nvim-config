66470347c11e7e3d6d5658e1933eaf454782ce2a
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-24 19:43:16 +0100

    disable arrow keys

--- lua/general/bindings.lua
+++ lua/general/bindings.lua
@@ -3,3 +3,9 @@ vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-c>", ":%y+<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Esc>", ":noh<CR>", { silent = true })
+
+-- Reprogram brain
+vim.api.nvim_set_keymap("n", "<Right>", "", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Left>", "", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Up>", "", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Down>", "", { noremap = true, silent = true })
