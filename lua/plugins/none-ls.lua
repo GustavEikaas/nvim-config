36ab8bf73c304b75498409590d8312263d753308
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-24 10:40:16 +0100

    add prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,7 +3,8 @@ return {
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
-      null_ls.builtins.formatting.stylua
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier
     })
     vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
   end
