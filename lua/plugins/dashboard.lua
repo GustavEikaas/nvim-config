a3dd29840de840be8c1c3b6ab85c6687f7930ef6
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-24 19:08:08 +0100

    update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,23 +1,71 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    local logo = [[
-         _____           _              
-        / ____|         | |             
-        | |  __ _   _ ___| |_ __ ___   __
-        | | |_ | | | / __| __/ _` \ \ / /
-        | |__| | |_| \__ \ || (_| |\ V / 
-        \_____|\__,_|___/\__\__,_| \_/  
-      ]]
+  "goolord/alpha-nvim",
+  event = "VimEnter", -- load plugin after all configuration is set
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 
-		logo = string.rep("\n", 8) .. logo .. "\n\n"
-    require('dashboard').setup {
-      disable_move = true,
-      config = {
-        header = vim.split(logo, "\n")
-      }
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
     }
+
+    _Gopts = {
+      position = "center",
+      hl = "Type",
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "   Find file", ":cd $HOME/dotfiles | Telescope find_files<CR>"),
+      dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
+      dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("y", "   Git diff", ":Telescope git_status<CR>"),
+      dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
+      dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
+      dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
+      dashboard.button("q", "󰩈   Quit NVIM", ":qa<CR>")
+    }
+
+    dashboard.opts.opts.noautocmd = true
+    alpha.setup(dashboard.opts)
+
+    require('alpha').setup(dashboard.opts)
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'LazyVimStarted',
+      callback = function()
+        local stats = require('lazy').stats()
+        local count = (math.floor(stats.startuptime * 100) / 100)
+        dashboard.section.footer.val = {
+          "󱐌 " .. stats.count .. " plugins loaded in " .. count .. " ms",
+          " ",
+          "      Gustav Eikaas"
+        }
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
   end,
-  dependencies = { {'nvim-tree/nvim-web-devicons'}}
 }
