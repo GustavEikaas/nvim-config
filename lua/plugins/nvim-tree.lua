0f451e630edc1de894fa9ea673735894d5b141e7
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-24 08:50:00 +0100

    switch to nvim tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,86 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      filters = {
+        dotfiles = false,
+        exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
+      },
+      disable_netrw = true,
+      hijack_netrw = true,
+      hijack_cursor = true,
+      hijack_unnamed_buffer_when_opening = false,
+      sync_root_with_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      view = {
+        adaptive_size = true,
+        side = "right",
+        width = 30,
+        preserve_window_proportions = true,
+      },
+      git = {
+        enable = true,
+        ignore = true,
+      },
+      filesystem_watchers = {
+        enable = true,
+      },
+      actions = {
+        open_file = {
+          resize_window = true,
+        },
+      },
+      renderer = {
+        root_folder_label = false,
+        highlight_git = true,
+        highlight_opened_files = "all",
+
+        indent_markers = {
+          enable = false,
+        },
+
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+          },
+
+          glyphs = {
+            default = "󰈚",
+            symlink = "",
+            folder = {
+              default = "",
+              empty = "",
+              empty_open = "",
+              open = "",
+              symlink = "",
+              symlink_open = "",
+              arrow_open = "",
+              arrow_closed = "",
+            },
+            git = {
+              unstaged = "✗",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      },
+    })
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+  end,
+}
