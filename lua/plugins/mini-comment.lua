8fad9b6f6076938a4818242ed2c87c101fec10ff
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-24 08:23:10 +0100

    add comment

---
+++ lua/plugins/mini-comment.lua
@@ -0,0 +1,13 @@
+return {
+  'echasnovski/mini.comment', 
+  version = false,
+  config = function()
+    require("mini.comment").setup({
+       mappings = {
+        comment = '<leader>/',
+        comment_line = '<leader>/',
+        comment_visual = '<leader>/',
+      },
+    })
+  end
+}
