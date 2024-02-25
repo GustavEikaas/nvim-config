1f0daf413014af4126409f75597907d276fabb15
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-25 21:16:31 +0100

    add rename lsp

--- lua/lsp-config/bindings.lua
+++ lua/lsp-config/bindings.lua
@@ -7,6 +7,7 @@ function bindings.setup()
   vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
   vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+  vim.keymap.set("n", "<leader>ra", vim.lsp.buf.rename, {})
 end
 
 return bindings
