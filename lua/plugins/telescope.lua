141b1ed828b31c6506e79d007778424eb78f6d42
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-25 11:45:27 +0100

    add telescope frecency

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,36 +1,45 @@
 return {
   {
-	  'nvim-telescope/telescope.nvim',
-	  config = function()
-	    local builtin = require("telescope.builtin")
-	    vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-	    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    'nvim-telescope/telescope.nvim',
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, {})
-      vim.keymap.set("n", '<leader>fr', builtin.oldfiles, {})
+      -- vim.keymap.set("n", '<leader>fr', builtin.oldfiles, {})
 
       vim.keymap.set("n", '<leader>gt', builtin.git_status, {})
       vim.keymap.set("n", '<leader>gb', builtin.git_branches, {})
-	  end,
-	  dependencies = { 'nvim-lua/plenary.nvim' }
+    end,
+    dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {
     'nvim-telescope/telescope-ui-select.nvim',
     config = function()
       require("telescope").setup {
-      extensions = {
-        ["ui-select"] = {
-        require("telescope.themes").get_dropdown {
-        }
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
         }
       }
-    }
       require("telescope").load_extension("ui-select")
     end
   },
   {
     "nvim-telescope/telescope-github.nvim",
-    config = function ()
+    config = function()
       require("telescope").load_extension("gh")
+      -- TODO: add keybinds
+    end
+  },
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    config = function()
+      require("telescope").load_extension("frecency")
+      vim.keymap.set("n", "<leader><leader>", "<Cmd>Telescope frecency<CR><Esc>")
+      vim.keymap.set("n", "<leader>fr", "<Cmd>Telescope frecency<CR><Esc>")
     end
   }
 }
