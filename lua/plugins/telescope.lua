39771fc5eca39bb0d79994e4dee35993668f0985
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-25 11:37:39 +0100

    add gh telescope integration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,5 +26,11 @@ return {
     }
       require("telescope").load_extension("ui-select")
     end
+  },
+  {
+    "nvim-telescope/telescope-github.nvim",
+    config = function ()
+      require("telescope").load_extension("gh")
+    end
   }
 }
