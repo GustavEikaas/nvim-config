f5c203cbcea5c46d7bd24474b6f059bff2840280
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-26 16:52:59 +0100

    disable more keybinds

--- lua/editor/nvim-tree.lua
+++ lua/editor/nvim-tree.lua
@@ -25,8 +25,9 @@ return {
         vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
         vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
         vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
-        vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
-        vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+        -- vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+        -- use O instead
+        -- vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
         vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
         vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
         vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
