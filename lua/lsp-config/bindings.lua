3526d9e23e0a2bd345e39c1e2841d767d20c7948
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-26 17:13:25 +0100

    add signature help

--- lua/lsp-config/bindings.lua
+++ lua/lsp-config/bindings.lua
@@ -8,6 +8,8 @@ function bindings.setup()
   vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
   vim.keymap.set("n", "<leader>ra", vim.lsp.buf.rename, {})
+  vim.keymap.set("n", "<C-z>", vim.lsp.buf.signature_help, {})
+  vim.keymap.set("i", "<C-z>", vim.lsp.buf.signature_help, {})
 end
 
 return bindings
