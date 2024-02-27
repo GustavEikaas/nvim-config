cc6229698953aef6017ec1d2254202bad4473c62
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-27 17:15:57 +0100

    add working debugger!!

--- lua/lsp/nvim-dap.lua
+++ lua/lsp/nvim-dap.lua
@@ -1,27 +1,49 @@
 return {
   "mfussenegger/nvim-dap",
-  enabled = false,
+  enabled = true,
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
-
-    dap.listeners.before.attach.dapui_config = function()
+    dapui.setup()
+    dap.listeners.after.event_initialized["dapui_config"] = function()
       dapui.open()
     end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
+    dap.listeners.before.event_terminated["dapui_config"] = function()
       dapui.close()
     end
-    dap.listeners.before.event_exited.dapui_config = function()
+    dap.listeners.before.event_exited["dapui_config"] = function()
       dapui.close()
     end
 
+    vim.keymap.set("n", "<F5>", dap.continue, {})
+    vim.keymap.set("n", "<F10>", dap.step_over, {})
+    vim.keymap.set("n", "<F11>", dap.step_into, {})
+    vim.keymap.set("n", "<F12>", dap.step_out, {})
     vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {})
-    vim.keymap.set("n", "<leader>dc", dap.continue, {})
+
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = "C:/Users/Gustav/AppData/Local/netcoredbg/netcoredbg.exe",
+      args = { "--interpreter=vscode" }
+    }
+
+    dap.configurations.cs = { {
+      type = "coreclr",
+      name = "launch - netcoredbg",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to dll", vim.fn.getcwd(), "file")
+      end
+    } }
   end,
   dependencies = {
-    "rcarriga/nvim-dap-ui"
+    "rcarriga/nvim-dap-ui",
+    {
+      "theHamsta/nvim-dap-virtual-text",
+      config = function ()
+        require("nvim-dap-virtual-text").setup()
+      end
+
+    }
   }
 }
