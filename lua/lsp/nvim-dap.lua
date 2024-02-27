72bfbb0cbb91648c0344759c18c977d49eb15051
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-27 17:39:57 +0100

    tweak dap ui

--- lua/lsp/nvim-dap.lua
+++ lua/lsp/nvim-dap.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
-    dapui.setup()
+    -- dapui.setup()
     dap.listeners.after.event_initialized["dapui_config"] = function()
       dapui.open()
     end
@@ -37,10 +37,83 @@ return {
     } }
   end,
   dependencies = {
-    "rcarriga/nvim-dap-ui",
+    {
+      "rcarriga/nvim-dap-ui",
+      config = function()
+        require("dapui").setup({
+          icons = { expanded = "", collapsed = "", current_frame = "" },
+          mappings = {
+            -- Use a table to apply multiple mappings
+            expand = { "<CR>", "<2-LeftMouse>" },
+            open = "o",
+            remove = "d",
+            edit = "e",
+            repl = "r",
+            toggle = "t",
+          },
+          element_mappings = {},
+          expand_lines = vim.fn.has("nvim-0.7") == 1,
+          force_buffers = true,
+          layouts = {
+            {
+              -- You can change the order of elements in the sidebar
+              elements = {
+                -- Provide IDs as strings or tables with "id" and "size" keys
+                { id = "breakpoints", size = 0.25},
+                {
+                  id = "scopes",
+                  size = 0.75, -- Can be float or integer > 1
+                },
+              },
+
+              size = 10,
+              position = "bottom", -- Can be "left" or "right"
+            },
+            {
+              elements = {
+                "repl",
+                "console",
+                "stacks",
+                "watches"
+              },
+              size = 35,
+              position = "right", -- Can be "bottom" or "top"
+            },
+          },
+          floating = {
+            max_height = nil,
+            max_width = nil,
+            border = "single",
+            mappings = {
+              ["close"] = { "q", "<Esc>" },
+            },
+          },
+          controls = {
+            enabled = vim.fn.exists("+winbar") == 1,
+            element = "repl",
+            icons = {
+              pause = "",
+              play = "",
+              step_into = "",
+              step_over = "",
+              step_out = "",
+              step_back = "",
+              run_last = "",
+              terminate = "",
+              disconnect = "",
+            },
+          },
+          render = {
+            max_type_length = nil, -- Can be integer or nil.
+            max_value_lines = 100, -- Can be integer or nil.
+            indent = 1,
+          },
+        })
+      end
+    },
     {
       "theHamsta/nvim-dap-virtual-text",
-      config = function ()
+      config = function()
         require("nvim-dap-virtual-text").setup()
       end
 
