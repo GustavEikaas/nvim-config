ed6f2d3239770518ee02103c0c3a15329a8aa21e
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-02 09:46:32 +0100

    move plugins

--- lua/plugins/editor.lua
+++
@@ -1,5 +0,0 @@
-return {
-  require("editor.nvim-tree"),
-  require("editor.gitsigns"),
-  require("editor.nvterm")
-}
--- lua/plugins/ui.lua
+++
@@ -1,9 +0,0 @@
-return {
-  require("ui.fidget"),
-  require("ui.telescope"),
-  require("ui.treesitter"),
-  require("ui.dracula"),
-  require("ui.dashboard"),
-  require("ui.lualine"),
-  require("ui.dressing")
-}
