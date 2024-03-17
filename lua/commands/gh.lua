206782860b13c93adbba487a4bbc336d35aa9f65
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-17 13:54:48 +0100

    add issues and pr commands

--- lua/commands/gh.lua
+++ lua/commands/gh.lua
@@ -13,6 +13,16 @@ gh.setup = function()
     vim.cmd("DiffviewOpen origin/main")
   end, {})
 
+  vim.api.nvim_create_user_command('Prs', function()
+    vim.cmd("Octo pr list")
+  end, {})
+
+  vim.api.nvim_create_user_command('Issues', function()
+    vim.cmd("Octo issue list")
+  end, {})
+
+
+
   vim.api.nvim_create_user_command('PR', function()
     local handle = io.popen("gh pr view --json number -q .number")
     local value = handle:read("l")
