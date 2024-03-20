63a7068be5a7b02e7516c4eb8de8d9e1fa2223cf
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-20 21:42:13 +0100

    add lualine update when song changes

--- lua/spotify/spotify.lua
+++ lua/spotify/spotify.lua
@@ -1,32 +1,14 @@
-local function print_song()
-  local handle = io.popen("spt playback --status")
-  if handle == nil then
-    return
-  end
-  local value = handle:read("*a")
-  handle:close()
-  vim.notify(value)
-end
-
 local function toggle_playback()
   os.execute("spt playback --toggle")
-  print_song()
 end
 
 local function prev_song()
   os.execute("spt playback --previous")
-  print_song()
 end
 
 local function next_song()
   os.execute("spt playback --next")
-  print_song()
 end
-
-
-vim.keymap.set("n", "<leader>sn", next_song, { silent = true })       -- Skip the current track
-vim.keymap.set("n", "<leader>sp", prev_song, { silent = true })       -- Skip the current track
-vim.keymap.set("n", "<leader>st", toggle_playback, { silent = true }) -- Skip the current track
 
 local M = {
   opts = {
@@ -39,13 +21,25 @@ local M = {
   _status_line = ""
 }
 
-
 function M.status:start()
-  local timer = vim.loop.new_timer()
-  timer:start(1000, M.opts.status.update_interval, vim.schedule_wrap(function()
+  local function force_update()
     local cmd = "spt playback --status --format '" .. M.opts.status.format .. "'"
     vim.fn.jobstart(cmd, { on_stdout = self.on_event, stdout_buffered = true })
-  end))
+  end
+
+  vim.keymap.set("n", "<leader>sn", function()
+    next_song()
+    force_update()
+  end, { silent = true })
+
+  vim.keymap.set("n", "<leader>sp", function()
+    prev_song()
+    force_update()
+  end, { silent = true })
+
+  vim.keymap.set("n", "<leader>st", toggle_playback, { silent = true })
+  local timer = vim.loop.new_timer()
+  timer:start(1000, M.opts.status.update_interval, vim.schedule_wrap(force_update))
 end
 
 function M.status:on_event(data)
