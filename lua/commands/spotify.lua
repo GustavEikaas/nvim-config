fdc0ad84507c6b91085b5d8330a5473391d4f498
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-20 22:01:49 +0100

    add random flag to spotify playlist command

--- lua/commands/spotify.lua
+++ lua/commands/spotify.lua
@@ -1,13 +1,13 @@
 local spotify = {}
 spotify.setup = function()
   vim.api.nvim_create_user_command('Metal', function()
-    os.execute("spt play --uri spotify:playlist:2Wk3JJp8MnQyKV1Uvr4Y0D ")
+    os.execute("spt play --uri spotify:playlist:2Wk3JJp8MnQyKV1Uvr4Y0D --random")
   end, {})
   vim.api.nvim_create_user_command('Country', function()
-    os.execute("spt play --uri spotify:playlist:7Lv78RoIVNlT7DidA4U4MW")
+    os.execute("spt play --uri spotify:playlist:7Lv78RoIVNlT7DidA4U4MW --random")
   end, {})
   vim.api.nvim_create_user_command('Rave', function()
-    os.execute("spt play --uri spotify:playlist:6zmx9wViGZuF5AR1vDAdA3")
+    os.execute("spt play --uri spotify:playlist:6zmx9wViGZuF5AR1vDAdA3 --random")
   end, {})
 end
 
