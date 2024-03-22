456bf767b53d021ffe1b005c07da97a487cc4ca3
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-22 21:56:49 +0100

    refactor lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -20,17 +20,38 @@ return {
       end,
       color = { fg = "#FFFFFF", bg = "#1DB954" }
     }
-
+    local pin_indicator = {
+      function()
+        local cur_buf = vim.api.nvim_get_current_buf()
+        return require("hbac.state").is_pinned(cur_buf) and "" or ""
+      end,
+      color = { fg = "#ef5f6b", gui = "bold" },
+    }
+    local buffer_line = {
+      'buffers',
+      max_length = vim.o.columns * 2 / 3,
+      filetype_names = {
+        TelescopePrompt = 'Telescope',
+        dashboard = 'Dashboard',
+        packer = 'Packer',
+        fzf = 'FZF',
+        alpha = 'Alpha',
+        NvimTree = "Tree"
+      },
+      use_mode_colors = false,
+      symbols = {
+        modified = ' ●',
+        alternate_file = '',
+        directory = '',
+      },
+    }
     require("lualine").setup({
       options = {
         icons_enabled = true,
         theme = 'dracula',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-          statusline = { "NvimTree" },
-          winbar = { "NvimTree" },
-        },
+        disabled_filetypes = { statusline = { "NvimTree" }, winbar = { "NvimTree" } },
         ignore_focus = { "NvimTree" },
         always_divide_middle = true,
         globalstatus = true,
@@ -45,47 +66,11 @@ return {
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = {},
         lualine_x = { 'encoding', 'filetype' },
-        lualine_y = {
-          {
-            function()
-              local cur_buf = vim.api.nvim_get_current_buf()
-              return require("hbac.state").is_pinned(cur_buf) and "" or ""
-            end,
-            color = { fg = "#ef5f6b", gui = "bold" },
-          }
-        },
+        lualine_y = { pin_indicator },
         lualine_z = { spotify_line }
       },
-      tabline = {
-        lualine_a = {
-          {
-            'buffers',
-            max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-            filetype_names = {
-              TelescopePrompt = 'Telescope',
-              dashboard = 'Dashboard',
-              packer = 'Packer',
-              fzf = 'FZF',
-              alpha = 'Alpha',
-              NvimTree = "Tree"
-            },
-            use_mode_colors = false,
-            symbols = {
-              modified = ' ●',
-              alternate_file = '',
-              directory = '',
-            },
-          }
-        }
-      },
-      winbar = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
-      },
+      tabline = { lualine_a = { buffer_line } },
+      winbar = {},
       inactive_winbar = {},
       extensions = {}
     })
