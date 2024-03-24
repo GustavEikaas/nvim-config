70e1a1b63a0edb21516d60655ef2bdd700fae2cd
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-24 20:38:03 +0000

    uncomment cmd for lua language server only works on windows

--- lua/lsp-config/lua_ls.lua
+++ lua/lsp-config/lua_ls.lua
@@ -1,11 +1,11 @@
 local lua_ls = {}
 
-
 function lua_ls.setup()
   local lspconfig = require("lspconfig")
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
   lspconfig.lua_ls.setup({
-    cmd = { "lua-language-server.cmd", "--stdio" },
+    -- Doesnt work on linux
+    -- cmd = { "lua-language-server.cmd", "--stdio" },
     capabilities = capabilities
   })
 end
