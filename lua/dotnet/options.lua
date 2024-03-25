0480600a031d6598f37fb1fad202d3cb8d7fd5b7
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-25 18:10:50 +0000

    fix secrets path for linux

--- lua/dotnet/options.lua
+++ lua/dotnet/options.lua
@@ -1,11 +1,18 @@
+local platform = vim.loop.os_uname().sysname
+
 return {
   secrets = {
     on_select = function(selectedItem)
       local home_dir = vim.fn.expand('~')
       --TODO: Make normalize path function to handle linux/windows
-      local secret_path = home_dir ..
-          '\\AppData\\Roaming\\Microsoft\\UserSecrets\\' .. selectedItem.secrets .. "\\secrets.json"
-      vim.cmd("edit " .. vim.fn.fnameescape(secret_path))
+      if platform == "Windows" then
+        local secret_path = home_dir ..
+            '\\AppData\\Roaming\\Microsoft\\UserSecrets\\' .. selectedItem.secrets .. "\\secrets.json"
+        vim.cmd("edit " .. vim.fn.fnameescape(secret_path))
+      else
+        local secret_path = home_dir .. "/.microsoft/usersecrets/" .. selectedItem.secrets .. "/secrets.json"
+        vim.cmd("edit " .. vim.fn.fnameescape(secret_path))
+      end
     end
   },
   run_project = {
