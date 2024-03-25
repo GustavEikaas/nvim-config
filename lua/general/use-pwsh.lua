06da37d3622f6a6315c0a69e47fed83e97ecf447
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-25 18:10:21 +0000

    use zsh as shell

--- lua/general/use-pwsh.lua
+++ lua/general/use-pwsh.lua
@@ -1,6 +1,7 @@
 local powershell_options = {
   shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
   shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
   shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
   shellquote = "",
@@ -9,9 +10,10 @@ local powershell_options = {
 
 local platform = vim.loop.os_uname().sysname
 if platform == "Windows" then
-for option, value in pairs(powershell_options) do
-  vim.opt[option] = value
-end
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
   vim.opt.shell = "powershell"
+else
+  vim.opt.shell = "/usr/bin/zsh"
 end
-
