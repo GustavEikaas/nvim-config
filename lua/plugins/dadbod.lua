f847440db14c6e8ec39c20e6717727daaab78e51
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-03-30 15:00:26 +0000

    qol dadbod

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -12,6 +12,19 @@ return {
   },
   init = function()
     -- Your DBUI configuration
+    vim.g.db_ui_winwidth = 40
+    vim.g.db_ui_use_nvim_notify = 1
+    vim.g.db_ui_icons = {
+      expanded = '',
+      collapsed = '',
+      saved_query = '',
+      new_query = '󰎔',
+      tables = '󰓫',
+      buffers = '',
+      connection_ok = '✓',
+      connection_error = '✕',
+    }
+
     vim.g.db_ui_use_nerd_fonts = 1
   end,
 }
