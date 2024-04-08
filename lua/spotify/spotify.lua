5e9e6aafa13961ba5b9c993e71aef8ff779a632d
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-04-08 20:00:27 +0200

    force_update song spotify

--- lua/spotify/spotify.lua
+++ lua/spotify/spotify.lua
@@ -1,15 +1,3 @@
-local function toggle_playback()
-  vim.fn.jobstart("spt playback --toggle")
-end
-
-local function prev_song()
-  vim.fn.jobstart("spt playback --previous")
-end
-
-local function next_song()
-  vim.fn.jobstart("spt playback --next")
-end
-
 local M = {
   opts = {
     status = {
@@ -26,15 +14,36 @@ function M.status:start()
     local cmd = "spt playback --status --format '" .. M.opts.status.format .. "'"
     vim.fn.jobstart(cmd, { on_stdout = self.on_event, stdout_buffered = true })
   end
+  local function toggle_playback()
+    vim.fn.jobstart("spt playback --toggle", {
+      on_exit = function()
+        force_update()
+      end
+    })
+  end
+
+  local function prev_song()
+    vim.fn.jobstart("spt playback --previous", {
+      on_exit = function()
+        force_update()
+      end
+    })
+  end
+
+  local function next_song()
+    vim.fn.jobstart("spt playback --next", {
+      on_exit = function()
+        force_update()
+      end
+    })
+  end
 
   vim.keymap.set("n", "<leader>sn", function()
     next_song()
-    force_update()
   end, { silent = true })
 
   vim.keymap.set("n", "<leader>sp", function()
     prev_song()
-    force_update()
   end, { silent = true })
 
   vim.keymap.set("n", "<leader>st", toggle_playback, { silent = true })
