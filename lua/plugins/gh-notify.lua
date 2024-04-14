01baf38d7b3cbcbf963a0ddf7e9b523a31f14757
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-04-14 14:18:17 +0200

    remove unused commands

--- lua/plugins/gh-notify.lua
+++ lua/plugins/gh-notify.lua
@@ -1,14 +1,9 @@
 return {
   dir = "C:\\Users\\Gustav\\repo\\gh-notify.nvim",
   config = function()
-    require("gh-notify").setup({
+    local gh = require("gh-notify")
+    gh.setup({
       polling = true
     })
-    vim.api.nvim_create_user_command('Sub', function()
-      require("gh-notify"):start()
-    end, {})
-    vim.api.nvim_create_user_command('GhNot', function()
-      require("gh-notify").list_messages()
-    end, {})
   end
 }
