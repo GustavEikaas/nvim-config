0cf639b845a0c1f6d9841c13d9f8c9003b2c2488
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-04-15 19:04:53 +0200

    remove auto checks on pr in octo window

--- after/ftplugin/octo.lua
+++ after/ftplugin/octo.lua
@@ -1,7 +1,6 @@
 -- Adds auto complete for issues and people tagging
 vim.keymap.set("i", "@", "@<C-x><C-o>", { silent = true, buffer = true })
 vim.keymap.set("i", "#", "#<C-x><C-o>", { silent = true, buffer = true })
-vim.cmd("Octo pr checks")
 vim.keymap.set("n", "<leader>c", function()
   vim.cmd("Octo pr checks")
 end, { silent = true, buffer = true })
