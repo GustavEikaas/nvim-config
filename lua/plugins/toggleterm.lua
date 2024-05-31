89e3f9534409c3bfacac05b33c5d3efc947577d9
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-05-31 23:18:49 +0200

    use lua instead of commadn

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup()
-    vim.keymap.set({ 'n', 't' }, '<A-i>', function() vim.cmd("ToggleTerm direction=float name=PWSH") end)
+    vim.keymap.set({ 'n', 't' }, '<A-i>', function() require("toggleterm").toggle(nil, nil, nil, "float") end)
   end,
 
 }
