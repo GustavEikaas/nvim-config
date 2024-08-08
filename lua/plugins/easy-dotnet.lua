e65c9678e2781eecfda793bef27b04c2a956a557
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-08-08 20:41:27 +0200

    wip

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -1,3 +1,31 @@
+local function parseCountersLine(xml)
+  local pattern =
+  '<Counters%s+total="(%d+)"%s+executed="(%d+)"%s+passed="(%d+)"%s+failed="(%d+)"%s+error="(%d+)"%s+timeout="(%d+)"%s+aborted="(%d+)"%s+inconclusive="(%d+)"%s+passedButRunAborted="(%d+)"%s+notRunnable="(%d+)"%s+notExecuted="(%d+)"%s+disconnected="(%d+)"%s+warning="(%d+)"%s+completed="(%d+)"%s+inProgress="(%d+)"%s+pending="(%d+)"%s*/>'
+
+  for total, executed, passed, failed, error, timeout, aborted, inconclusive, passedButRunAborted, notRunnable, notExecuted, disconnected, warning, completed, inProgress, pending in xml:gmatch(pattern) do
+    return {
+      total = tonumber(total),
+      executed = tonumber(executed),
+      passed = tonumber(passed),
+      failed = tonumber(failed),
+      error = tonumber(error),
+      timeout = tonumber(timeout),
+      aborted = tonumber(aborted),
+      inconclusive = tonumber(inconclusive),
+      passedButRunAborted = tonumber(passedButRunAborted),
+      notRunnable = tonumber(notRunnable),
+      notExecuted = tonumber(notExecuted),
+      disconnected = tonumber(disconnected),
+      warning = tonumber(warning),
+      completed = tonumber(completed),
+      inProgress = tonumber(inProgress),
+      pending = tonumber(pending)
+    }
+  end
+
+  return nil
+end
+
 local function parseXmlLine(xml)
   local results = {}
   local pattern = '<UnitTestResult[^>]*testName="([^"]*)"[^>]*outcome="([^"]*)"[^>]*>'
@@ -19,7 +47,10 @@ local function parseXmlLine(xml)
 end
 
 local function parseXmlFile(filepath)
-  local results = {}
+  local results = {
+    summary = {},
+    tests = {}
+  }
 
   local file = io.open(filepath, "r")
   if not file then
@@ -30,7 +61,12 @@ local function parseXmlFile(filepath)
   for line in file:lines() do
     local res = parseXmlLine(line)
     for _, val in ipairs(res) do
-      table.insert(results, val)
+      table.insert(results.tests, val)
+    end
+
+    local counters = parseCountersLine(line)
+    if counters ~= nil then
+      results.summary = counters
     end
   end
 
@@ -105,6 +141,76 @@ local function groupByClassName(results)
 
   return grouped
 end
+local function formatCounters(counters)
+  local sequence = {
+    "total", "executed", "passed", "failed", "error", "timeout",
+    "aborted", "inconclusive", "passedButRunAborted", "notRunnable",
+    "notExecuted", "disconnected", "warning", "completed",
+    "inProgress", "pending"
+  }
+
+  local parts = {}
+
+  for _, key in ipairs(sequence) do
+    local value = counters[key]
+    if value and value ~= 0 then
+      table.insert(parts, string.format("%s: %d", key:gsub("^%l", string.upper), value))
+    end
+  end
+
+  return table.concat(parts, ", ")
+end
+
+local function linesForSummary(counters, buf)
+  local lines = {}
+  table.insert(lines, "Summary")
+  table.insert(lines, string.format("%s: %d", "Total", counters.total))
+  table.insert(lines, string.format("%s: %d", "Passed", counters.passed))
+  table.insert(lines, string.format("%s: %d", "Failed", counters.failed))
+  table.insert(lines, "")
+
+  vim.api.nvim_buf_set_lines(buf, -1, -1, true, lines)
+
+  -- Apply highlights
+  vim.api.nvim_buf_add_highlight(buf, -1, "Character", 3, string.len("Passed: "), -1)
+  vim.api.nvim_buf_add_highlight(buf, -1, "DiagnosticError", 4, string.len("Failed: "), -1)
+  return #lines
+end
+
+local function render_test_results()
+  local debugger = require("general.debug")
+  local res = parseXmlFile("C:/Users/Gustav/repo/NeovimDebugProject/src/NeovimDebugProject.IntegrationTests/TestResults/easy-dotnet.trx")
+
+  if res == nil then
+    error("No output from xml file")
+  end
+  -- Create a new buffer
+  local buf = vim.api.nvim_create_buf(false, true) -- false for not listing, true for scratch
+
+  -- Set the new buffer as the current buffer in a new window
+  vim.api.nvim_set_current_buf(buf)
+
+  local grouped = groupByClassName(res.tests)
+  -- Insert text into the new buffer
+
+  local indexOffset = linesForSummary(res.summary, buf)
+
+  local lines = {}
+  for key, testResults in pairs(grouped) do
+    table.insert(lines, key)
+    -- Iterate results here
+    for _, testResult in pairs(testResults) do
+      table.insert(lines, "  " .. getOutcome(testResult.outcome) .. " - " .. testResult.testName)
+    end
+    table.insert(lines, "")
+  end
+
+  vim.api.nvim_buf_set_lines(buf, -1, -1, true, lines)
+
+  vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+  vim.api.nvim_buf_set_name(buf, 'easy-dotnet test results')
+  vim.api.nvim_buf_set_option(buf, "filetype", "easy-dotnet")
+end
 
 return {
   "GustavEikaas/easy-dotnet.nvim",
@@ -120,7 +226,7 @@ return {
             return "dotnet run --project " .. path
           end,
           test = function()
-            return "dotnet test " .. path
+            return "dotnet test " .. path .. ' --logger "trx;LogFileName=easy-dotnet.trx"'
           end,
           restore = function()
             return "dotnet restore " .. path
@@ -148,12 +254,7 @@ return {
           vim.notify("Tests started")
           vim.fn.jobstart(command, {
             on_exit = function(_, b, _)
-              if b == 0 then
-                vim.notify("All tests passed")
-              else
-                vim.notify("Tests failed")
-                populate_quickfix_from_file(logPath)
-              end
+              render_test_results()
             end,
           })
         else
@@ -166,36 +267,6 @@ return {
 
     vim.api.nvim_create_user_command('Secrets', function()
       dotnet.secrets()
-    end, {})
-
-    vim.api.nvim_create_user_command("T", function()
-      local debugger = require("general.debug")
-      local res = parseXmlFile(
-        "C:/Users/Gustav/repo/NeovimDebugProject/src/NeovimDebugProject.IntegrationTests/TestResults/easy-dotnet.trx")
-      if res == nil then
-        error("No output from xml file")
-      end
-      -- debugger.write_to_log(res)
-      -- Create a new buffer
-      local buf = vim.api.nvim_create_buf(false, true) -- false for not listing, true for scratch
-
-      -- Set the new buffer as the current buffer in a new window
-      vim.api.nvim_set_current_buf(buf)
-
-      local grouped = groupByClassName(res)
-      -- Insert text into the new buffer
-      local lines = {}
-
-      for key, testResults in pairs(grouped) do
-        table.insert(lines, key)
-        -- Iterate results here
-        for _, testResult in pairs(testResults) do
-          table.insert(lines, "  " .. getOutcome(testResult.outcome) .. " - " .. testResult.testName)
-        end
-        table.insert(lines, "")
-      end
-
-      vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
     end, {})
 
     -- Temp
