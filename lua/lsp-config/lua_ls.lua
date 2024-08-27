878b1da56169c926a1859d2e1a65d5b7a6e3f4eb
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-08-27 22:13:38 +0200

    add ext_lib typings

--- lua/lsp-config/lua_ls.lua
+++ lua/lsp-config/lua_ls.lua
@@ -3,6 +3,9 @@ local lua_ls = {}
 function lua_ls.setup()
   local lspconfig = require("lspconfig")
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  local runtime_path = vim.split(package.path, ';')
+  table.insert(runtime_path, "lua/?.lua")
+  table.insert(runtime_path, "lua/?/init.lua")
   lspconfig.lua_ls.setup({
     cmd = require("extensions").isWindows() and { "lua-language-server.cmd", "--stdio" } or nil,
     capabilities = capabilities,
@@ -10,13 +13,18 @@ function lua_ls.setup()
       Lua = {
         runtime = {
           version = "LuaJIT",
-          path = vim.split(package.path, ";"),
+          path = runtime_path
         },
         diagnostics = {
           globals = { "vim" },
         },
         workspace = {
-          library = { vim.env.VIMRUNTIME },
+          library = {
+            vim.env.VIMRUNTIME,
+            vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "easy-dotnet.nvim"),
+            vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "code-playground.nvim"),
+            vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "gh-actions.nvim"),
+          },
           checkThirdParty = false,
         },
         telemetry = {
