19f5584d09549e5fcfda69c5093c937d8f3b7689
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2024-08-27 08:37:15 +0200

    Update mason.lua

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -15,7 +15,7 @@ return
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "powershell_es", "yamlls", "rust_analyzer" }
+        ensure_installed = { "lua_ls", "tsserver", "powershell_es", "yamlls", "rust_analyzer", "fsautocomplete" }
       })
     end
   },
