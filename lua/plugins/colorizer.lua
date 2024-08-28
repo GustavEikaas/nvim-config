49e401e183128533f6801f9c208ae0530f60c345
Gustav Eikaas <guei@equinor.com>
2024-08-28 14:37:10 +0200

    feat: disable color hl for names

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,6 +2,9 @@ return {
   "norcalli/nvim-colorizer.lua",
   event = "BufRead",
   config = function()
-    require("colorizer").setup()
+    require("colorizer").setup({ "*" }, {
+      names = false,        -- "Name" codes like Blue
+      mode  = 'background', -- Set the display mode.
+    })
   end
 }
