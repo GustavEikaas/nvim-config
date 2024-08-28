1ea48b6982ca3bff6ab5e01d7ff53a2d134381ac
Gustav Eikaas <guei@equinor.com>
2024-08-28 14:43:10 +0200

    cleanup

--- lua/plugins/easy-git.lua
+++ lua/plugins/easy-git.lua
@@ -7,10 +7,6 @@ return {
     local git = require("easy-git")
     git.setup()
 
-    vim.api.nvim_create_user_command('B', git.pick_branch, {})
     vim.api.nvim_create_user_command('GGRF', git.restore_current_file, {})
-    vim.api.nvim_create_user_command("GGRM", git.reset_to_main, {})
-    vim.api.nvim_create_user_command("GGR", git.reset_branch, {})
-    vim.keymap.set("n", "<C-b>", git.pick_branch, { noremap = true, silent = true })
   end
 }
