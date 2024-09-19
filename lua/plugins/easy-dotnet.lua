86d704866ab3c28de268f1619cb3848600a20203
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-09-19 22:08:14 +0200

    .

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -21,6 +21,7 @@ end
 return {
   "GustavEikaas/easy-dotnet.nvim",
   -- dir = "C:\\Users\\Gustav\\repo\\easy-dotnet.nvim",
+  branch = "feat/test-debugger-netcore",
   dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
   config = function()
     local dotnet = require("easy-dotnet")
