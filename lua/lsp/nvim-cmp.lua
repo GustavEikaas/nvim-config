de2c29ccef201728f4c40a9668ec4177a94f8816
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-09-22 20:48:08 +0200

    chore(lockfile): update lockfile

--- lua/lsp/nvim-cmp.lua
+++ lua/lsp/nvim-cmp.lua
@@ -17,18 +17,16 @@ return {
   },
   config = function()
     local cmp = require 'cmp'
+
     local luasnip = require("luasnip")
     require("luasnip.loaders.from_vscode").lazy_load()
+    cmp.register_source("easy-dotnet", require("easy-dotnet").package_completion_source)
 
     cmp.setup({
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          require('luasnip').lsp_expand(args.body)
         end,
-      },
-      window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -61,10 +59,11 @@ return {
       sources = cmp.config.sources({
         { name = 'nvim_lsp',              priority = 1000 },
         { name = 'luasnip',               priority = 750 },
+        { name = 'easy-dotnet' },
         { name = "vim-dadbod-completion", priority = 700 },
         { name = "buffer",                priority = 500 },
         { name = "path",                  priority = 250 },
-      })
+      }),
     })
 
     cmp.setup.filetype('octo', {
