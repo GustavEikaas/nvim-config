4cd27bb00b7d3ea46fe65f8c2a43473e5808754e
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-10-19 18:37:00 +0200

    disable vim.notify

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -100,7 +100,7 @@ return {
         -- event is always "notify" and kind can be any log level as a string
         -- The default routes will forward notifications to nvim-notify
         -- Benefit of using Noice for this is the routing and consistent history view
-        enabled = true,
+        enabled = false,
         view = "notify",
       },
       lsp = {
