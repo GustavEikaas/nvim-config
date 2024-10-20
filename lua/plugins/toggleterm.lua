959427456e34422908daf95f0b7afef32bfb62b1
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-10-20 11:04:53 +0200

    add double terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,8 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup()
-    vim.keymap.set({ 'n', 't' }, '<A-i>', function() require("toggleterm").toggle(nil, nil, nil, "float") end)
+    vim.keymap.set({ 'n', 't' }, '<A-i>', function() require("toggleterm").toggle(1, nil, nil, "float") end)
+    vim.keymap.set({ 'n', 't' }, '<A-h>', function() require("toggleterm").toggle(2, nil, nil, "horizontal") end)
   end,
 
 }
