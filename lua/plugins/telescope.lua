bc888681bf867c33f4cbac9990fefe96b7aa1754
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-11-22 21:09:21 +0100

    .

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,16 +2,6 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     event = "VeryLazy",
-    config = function()
-      local builtin = require("telescope.builtin")
-      -- vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-      -- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      -- vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, {})
-      vim.keymap.set("n", '<leader>fr', builtin.oldfiles, {})
-      vim.keymap.set("n", '<leader>gt', builtin.git_status, {})
-      vim.keymap.set("n", '<leader>gb', builtin.git_branches, {})
-    end,
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {
@@ -21,20 +11,11 @@ return {
       require("telescope").setup {
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
+            require("telescope.themes").get_dropdown {}
           }
         }
       }
       require("telescope").load_extension("ui-select")
-    end
-  },
-  {
-    "nvim-telescope/telescope-github.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("telescope").load_extension("gh")
-      -- TODO: add keybinds
     end
   }
 }
