be15a75270e8d85dfce795a55d03d43bd2bc586a
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-11-22 21:20:19 +0100

    fix conflicting keybind with mini.move

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,8 +2,10 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup()
-    vim.keymap.set({ 'n', 't' }, '<A-i>', function() require("toggleterm").toggle(1, nil, nil, "float") end)
-    vim.keymap.set({ 'n', 't' }, '<A-h>', function() require("toggleterm").toggle(2, nil, nil, "horizontal") end)
+    vim.keymap.set({ 'n', 't' }, '<A-i>', function() require("toggleterm").toggle(1, nil, nil, "float") end,
+      { noremap = true, silent = true })
+    vim.keymap.set({ 'n', 't' }, '<A-h>', function() require("toggleterm").toggle(2, nil, nil, "horizontal") end,
+      { noremap = true, silent = true })
 
     function _G.set_terminal_keymaps()
       local opts = { buffer = 0 }
