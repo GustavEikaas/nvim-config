6c3b4fa78e69acd78dc6d9b810fea002c35b756a
GustavEikaas <gustav.eikaas@gmail.com>
2024-12-21 11:33:05 +0100

    revert

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -20,7 +20,7 @@ end
 
 return {
   "GustavEikaas/easy-dotnet.nvim",
-  dir = "C:\\Users\\Gusta\\repo\\easy-dotnet.nvim",
+  -- dir = "C:\\Users\\Gusta\\repo\\easy-dotnet.nvim",
   dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
   config = function()
     local dotnet = require("easy-dotnet")
