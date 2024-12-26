36bba3a3de273c01576509e57c18a6e942f5afd3
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-12-27 00:05:46 +0100

    feat: use conform.nvim

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        -- cs = { "csharpier" },
+      },
+    }
+    vim.keymap.set("n", "<leader>fm", require("conform").format, {})
+  end,
+}
