81604dbf85455181b9e17cc640a8fd59f6dc1475
Gustav Eikaas <gustav.eikaas@gmail.com>
2025-03-23 09:39:34 +0100

    feat: graceful buf cycle

--- lua/buf-cycle.lua
+++ lua/buf-cycle.lua
@@ -1,23 +1,56 @@
 local M = {
-  ring_buf = vim.ringbuf(5)
+  buffer_list = {},
 }
 
-M.setup = function()
-  local hbac = require("hbac")
+local function gracefully_delete_buffer(bufnr)
+  if bufnr and vim.api.nvim_buf_is_valid(bufnr) then
+    if vim.api.nvim_buf_get_option(bufnr, "modified") then
+      return
+    end
+    pcall(vim.api.nvim_buf_delete, bufnr, {})
+  end
+end
 
-  hbac.setup({
-    autoclose                  = true,
-    threshold                  = 1,
-    close_command              = function(bufnr)
-      local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+M.setup = function()
+  local hbac = require "hbac"
+
+  hbac.setup {
+    autoclose = true,
+    threshold = 1,
+    close_command = function(bufnr)
+      local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
       if filetype == "octo" then
         return
       end
-      M.ring_buf:push(vim.api.nvim_buf_get_name(bufnr))
-      vim.api.nvim_buf_delete(bufnr, {})
+
+      local buf_name = vim.api.nvim_buf_get_name(bufnr)
+
+      local dupe = false
+      for _, value in ipairs(M.buffer_list) do
+        if value == buf_name then
+          dupe = true
+        end
+      end
+
+      if dupe == true then
+        gracefully_delete_buffer(bufnr)
+        return
+      end
+
+      if vim.tbl_contains(M.buffer_list, buf_name) then
+        --prevent dupes
+        return
+      end
+      table.insert(M.buffer_list, buf_name)
+
+      if #M.buffer_list > 7 then
+        table.remove(M.buffer_list) -- Remove last (oldest) element
+      end
+
+      gracefully_delete_buffer(bufnr)
     end,
     close_buffers_with_windows = false,
-  })
+  }
 
   vim.keymap.set("n", "<leader>a", function()
     hbac.toggle_pin()
@@ -25,24 +58,34 @@ M.setup = function()
   end, { noremap = true, silent = true })
 
   vim.keymap.set("n", "<C-x>", function()
-    vim.cmd("Hbac unpin_all")
-    vim.cmd("Hbac close_unpinned")
+    vim.cmd "Hbac unpin_all"
+    vim.cmd "Hbac close_unpinned"
   end, { noremap = true, silent = true })
 
   local function gotoprevbuf()
-    local prev_buf = M.ring_buf:pop()
-    if prev_buf == vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) then
-      gotoprevbuf()
+    local last_index = #M.buffer_list
+    local prev_buf = M.buffer_list[last_index]
+    local prev_table = {}
+    local cur_buf = vim.api.nvim_get_current_buf()
+    local curr_buf_would_close = not require("hbac.state").is_pinned(cur_buf)
+    table.insert(prev_table, prev_buf)
+    if curr_buf_would_close then
+      table.insert(prev_table, vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
     end
-    if prev_buf then
-      vim.cmd('edit ' .. vim.fn.fnameescape(prev_buf))
-    else
-      print("No buffer to restore.")
+
+    for _, value in ipairs(M.buffer_list) do
+      if value ~= prev_buf and value ~= vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) then
+        table.insert(prev_table, value)
+      end
     end
+    M.buffer_list = prev_table
+
+    vim.cmd("edit " .. vim.fn.fnameescape(prev_buf))
   end
 
   vim.keymap.set("n", "<leader>T", function()
     gotoprevbuf()
   end, { noremap = true, silent = true })
 end
+
 return M
