fbadc4f547096c5fd55ad8620bd83be09337418c
GustavEikaas <gustav.eikaas@gmail.com>
2025-03-30 16:05:10 +0200

    feat: disable lualine branch for perf reasons

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,18 +10,6 @@ return {
       color = { fg = "#FFFFFF", bg = "#1DB954" },
     }
 
-    local buf_cycle = {
-      function()
-        local buf_cycle = require("buf-cycle").ring_buf
-        local path = buf_cycle:peek()
-        if not path then
-          return "-"
-        end
-        return vim.fn.fnamemodify(path, ":t")
-      end,
-      color = { bg = "#ef5f6b", fg = "#000000" },
-    }
-
     local pin_indicator = {
       function()
         local cur_buf = vim.api.nvim_get_current_buf()
@@ -29,6 +17,7 @@ return {
       end,
       color = { fg = "#ef5f6b", gui = "bold" },
     }
+
     local buffer_line = {
       "buffers",
       max_length = vim.o.columns * 2 / 3,
@@ -47,6 +36,7 @@ return {
         directory = "",
       },
     }
+
     require("lualine").setup {
       options = {
         icons_enabled = true,
@@ -65,13 +55,13 @@ return {
       },
       sections = {
         lualine_a = { "mode", debugger },
-        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_b = {},
         lualine_c = {},
         lualine_x = { "encoding", "filetype" },
         lualine_y = { pin_indicator },
         lualine_z = {},
       },
-      tabline = { lualine_a = { buffer_line }, lualine_z = { buf_cycle } },
+      tabline = { lualine_a = { buffer_line }, lualine_z = {  } },
       winbar = {},
       inactive_winbar = {},
       extensions = {},
