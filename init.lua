9acd2ca9f7386113b79fee234f7fcded5460c03a
GustavEikaas <gustav.eikaas@gmail.com>
2025-05-07 17:42:18 +0200

    rpc wip

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 ---@diagnostic disable-next-line: unused-local
-local extensions = require("extensions")
+local extensions = require "extensions"
 
 require "general.use-pwsh"
 require "general.reload-buf"
 require "general.bindings"
 
-require "commands.gh".setup()
-require "commands.markdown".setup()
-require "commands.todo".setup()
-require "commands.lsp".setup()
-require "commands.js".setup()
-require "commands.spotify".setup()
-require "commands.qol".setup()
+require("commands.gh").setup()
+require("commands.markdown").setup()
+require("commands.todo").setup()
+require("commands.lsp").setup()
+require("commands.js").setup()
+require("commands.spotify").setup()
+require("commands.qol").setup()
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -25,6 +25,50 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 vim.opt.rtp:prepend(lazypath)
-require("vim-options")
+require "vim-options"
 require("lazy").setup "plugins"
-vim.cmd('filetype plugin on')
+vim.cmd "filetype plugin on"
+
+local function wrap(callback)
+  return function(...)
+    -- Check if we are already in a coroutine
+    if coroutine.running() then
+      -- If already in a coroutine, call the callback directly
+      callback(...)
+    else
+      -- If not, create a new coroutine and resume it
+      local co = coroutine.create(callback)
+      local s = ...
+      local handle = function()
+        local success, err = coroutine.resume(co, s)
+        if not success then
+          print("Coroutine failed: " .. err)
+        end
+      end
+      handle()
+    end
+  end
+end
+
+vim.api.nvim_create_user_command("DotnetRunTest", function()
+
+  -- string VSTestPath { get; init; }
+  -- string DLLPath { get; init; }
+  -- string OutFile { get; init; }
+  local dllPath = "C:/Users/gusta/repo/NeovimDebugProject/NeovimDebugProject.Tests/bin/Debug/net9.0/NeovimDebugProject.Tests.dll"
+  local vstestDll = 'C:/Program Files/dotnet/sdk/9.0.203/vstest.console.dll'
+  local path = vim.fs.normalize(os.tmpname())
+  require("msg").send("VSTest_Discover", { vsTestPath = vstestDll, dllPath = dllPath, outFile = path}, function(response)
+    if response.error then
+      vim.schedule(function()
+        vim.notify(string.format("[%s]: %s", response.error.code, response.error.message), vim.log.levels.ERROR)
+      end)
+      return
+    end
+    vim.schedule(function()
+      vim.print(response)
+      local contents = vim.fn.readfile(path)
+      vim.print(contents)
+    end)
+  end)
+end, {})
---
+++ lua/msg.lua
@@ -0,0 +1,93 @@
+local M = {}
+
+local client = nil
+local connected = false
+local callbacks = {}
+
+local function get_pipe_path()
+  return "\\\\.\\pipe\\EasyDotnetPipe"
+end
+
+local function read_loop()
+  client:read_start(function(err, data)
+    if err then
+      vim.schedule(function()
+        vim.notify("Pipe read error: " .. err, vim.log.levels.ERROR)
+      end)
+      return
+    end
+
+    if data then
+      for line in data:gmatch "[^\r\n]+" do
+        local ok, decoded = pcall(vim.json.decode, line)
+        if ok and decoded and decoded.id then
+          local cb = callbacks[decoded.id]
+          if cb then
+            callbacks[decoded.id] = nil
+            vim.schedule(function()
+              cb(decoded)
+            end)
+          end
+        else
+          vim.schedule(function()
+            vim.notify("Malformed or unmatched response: " .. line, vim.log.levels.WARN)
+          end)
+        end
+      end
+    end
+  end)
+end
+
+local function connect_pipe(callback)
+  if connected and client and not client:is_closing() then
+    callback()
+    return
+  end
+
+  client = vim.loop.new_pipe(false)
+  client:connect(get_pipe_path(), function(err)
+    if err then
+      vim.schedule(function()
+        vim.notify("Failed to connect: " .. err, vim.log.levels.ERROR)
+      end)
+      return
+    end
+    connected = true
+    read_loop()
+    callback()
+  end)
+end
+
+local _id = 0
+local function get_command_id()
+  _id = _id + 1
+  return _id
+end
+
+function M.send(method, params, callback)
+  connect_pipe(function()
+    local id = get_command_id()
+    local message = {
+      id = id,
+      method = method,
+      params = params or {},
+    }
+    vim.schedule(function()
+      vim.print("Sending message", message)
+    end)
+    callbacks[id] = callback
+    client:write(vim.json.encode(message) .. "\n")
+  end)
+end
+
+function M.close()
+  if client and not client:is_closing() then
+    client:shutdown(function()
+      client:close()
+      client = nil
+      connected = false
+    end)
+  end
+end
+
+return M
