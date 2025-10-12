39a6f15c1c46f9e8f6c83804ab65740020e90e2f
GustavEikaas <gustav.eikaas@gmail.com>
2025-10-12 10:02:16 +0200

    feat: treesitter transition to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,5 @@
 return {
+
   {
     "windwp/nvim-ts-autotag",
     config = function()
@@ -11,81 +12,45 @@ return {
       }
     end,
   },
+
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+    branch = "main",
     build = ":TSUpdate",
-    config = function()
-      local config = require "nvim-treesitter.configs"
-      config.setup {
-        auto_install = true,
-        highlight = {
-          enable = true,
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-        },
-        indent = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
-
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              -- You can also use captures from other query groups like `locals.scm`
-              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-            },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
-            selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "V", -- linewise
-              ["@class.outer"] = "<c-v>", -- blockwise
-            },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true or false
-            include_surrounding_whitespace = true,
-          },
-        },
-      }
-      vim.treesitter.language.register("markdown", "octo")
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.fsharp = {
-        install_info = {
-          url = "https://github.com/ionide/tree-sitter-fsharp",
-          branch = "main",
-          files = { "src/scanner.c", "src/parser.c" },
-        },
-        requires_generate_from_grammar = false,
-        filetype = "fsharp",
-      }
-    end,
   },
+
+  -- {
+  --   "nvim-treesitter/nvim-treesitter-textobjects",
+  --   branch = "main",
+  --   config = function()
+  --     require("nvim-treesitter.configs").setup {
+  --       highlight = { enable = true },
+  --       indent = { enable = true },
+  --       autotag = {
+  --         enable = true,
+  --         enable_close = true,
+  --         enable_rename = true,
+  --         enable_close_on_slash = false,
+  --       },
+  --       textobjects = {
+  --         select = {
+  --           enable = true,
+  --           lookahead = true,
+  --           keymaps = {
+  --             ["af"] = "@function.outer",
+  --             ["if"] = "@function.inner",
+  --             ["ac"] = "@class.outer",
+  --             ["ic"] = "@class.inner",
+  --           },
+  --           selection_modes = {
+  --             ["@parameter.outer"] = "v",
+  --             ["@function.outer"] = "V",
+  --             ["@class.outer"] = "<c-v>",
+  --           },
+  --           include_surrounding_whitespace = true,
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
 }
