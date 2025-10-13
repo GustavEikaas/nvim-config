b2540286d3acd2eb910a5b66592ca3531e5d30bb
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2025-10-13 15:36:48 +0200

    feat: error silencing reload buf

--- lua/general/reload-buf.lua
+++ lua/general/reload-buf.lua
@@ -1,6 +1,6 @@
 -- auto reload file when contents are written from an external source. e.g github reset --hard
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
-  pattern = "*",
-  command = "if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
+  callback = function()
+    pcall(vim.cmd, "checktime")
+  end,
 })
-
