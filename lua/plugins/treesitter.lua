800fbe77248d3c5a0dc85ed5d4e9870ef5c76223
Gustav Eikaas <guei@equinor.com>
2025-10-17 13:19:21 +0200

    fix: treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     "windwp/nvim-ts-autotag",
     config = function()
@@ -12,45 +11,45 @@ return {
       }
     end,
   },
-
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     build = ":TSUpdate",
+    config = function()
+      vim.treesitter.language.register("tsx", { "typescriptreact", "javascriptreact" })
+      vim.treesitter.language.register("typescript", { "typescript", "javascript" })
+      require("nvim-treesitter.configs").setup {
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotag = {
+          enable = true,
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+            selection_modes = {
+              ["@parameter.outer"] = "v",
+              ["@function.outer"] = "V",
+              ["@class.outer"] = "<c-v>",
+            },
+            include_surrounding_whitespace = false,
+          },
+        },
+      }
+    end,
   },
-
-  -- {
-  --   "nvim-treesitter/nvim-treesitter-textobjects",
-  --   branch = "main",
-  --   config = function()
-  --     require("nvim-treesitter.configs").setup {
-  --       highlight = { enable = true },
-  --       indent = { enable = true },
-  --       autotag = {
-  --         enable = true,
-  --         enable_close = true,
-  --         enable_rename = true,
-  --         enable_close_on_slash = false,
-  --       },
-  --       textobjects = {
-  --         select = {
-  --           enable = true,
-  --           lookahead = true,
-  --           keymaps = {
-  --             ["af"] = "@function.outer",
-  --             ["if"] = "@function.inner",
-  --             ["ac"] = "@class.outer",
-  --             ["ic"] = "@class.inner",
-  --           },
-  --           selection_modes = {
-  --             ["@parameter.outer"] = "v",
-  --             ["@function.outer"] = "V",
-  --             ["@class.outer"] = "<c-v>",
-  --           },
-  --           include_surrounding_whitespace = true,
-  --         },
-  --       },
-  --     }
-  --   end,
-  -- },
 }
