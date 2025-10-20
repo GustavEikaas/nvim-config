e8bf94eb176452f0bb1d96b1a80759b420abb395
GustavEikaas <gustav.eikaas@gmail.com>
2025-10-20 19:41:23 +0200

    finally fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,91 +1,90 @@
+local function select(textobj)
+  return function()
+    require("nvim-treesitter-textobjects.select").select_textobject(textobj, "textobjects")
+  end
+end
+
 return {
   {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup {
-        opts = {
-          enable_close = true,
-          enable_rename = true,
-          enable_close_on_slash = false,
-        },
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = "main",
+    build = ":TSUpdate",
+    init = function()
+      local parser_installed = {
+        "python",
+        "go",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown_inline",
+        "markdown",
+        "c_sharp",
       }
+
+      vim.defer_fn(function()
+        require("nvim-treesitter").install(parser_installed)
+      end, 1000)
+      require("nvim-treesitter").update()
+
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "User: enable treesitter highlighting",
+        callback = function(ctx)
+          local hasStarted = pcall(vim.treesitter.start)
+
+          local noIndent = {}
+          if hasStarted and not vim.list_contains(noIndent, ctx.match) then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
     end,
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+
+    opts = {
+      select = {
+        lookahead = true,
+        include_surrounding_whitespace = false,
+      },
     },
-    build = ":TSUpdate",
-    config = function()
-      local config = require "nvim-treesitter.configs"
-      config.setup {
-        auto_install = true,
-        highlight = {
-          enable = true,
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-        },
-        indent = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
+    keys = {
+      -- MOVE
+      -- {
+      --   "<C-j>",
+      --   function()
+      --     local move = require "nvim-treesitter-textobjects.move"
+      --     move.goto_next_start("@function.outer", "textobjects")
+      --   end,
+      --   desc = " Goto next function",
+      -- },
+      -- {
+      --   "<C-k>",
+      --   function()
+      --     local move = require "nvim-treesitter-textobjects.move"
+      --     move.goto_previous_start("@function.outer", "textobjects")
+      --   end,
+      --   desc = " Goto prev function",
+      -- },
 
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              -- You can also use captures from other query groups like `locals.scm`
-              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-            },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
-            selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "V", -- linewise
-              ["@class.outer"] = "<c-v>", -- blockwise
-            },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true or false
-            include_surrounding_whitespace = true,
-          },
-        },
-      }
-      vim.treesitter.language.register("markdown", "octo")
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.fsharp = {
-        install_info = {
-          url = "https://github.com/ionide/tree-sitter-fsharp",
-          branch = "main",
-          files = { "src/scanner.c", "src/parser.c" },
-        },
-        requires_generate_from_grammar = false,
-        filetype = "fsharp",
-      }
-    end,
+      -- TEXT OBJECTS
+      { "a<CR>", select "@return.outer", mode = { "x", "o" }, desc = "↩ outer return" },
+      { "i<CR>", select "@return.inner", mode = { "x", "o" }, desc = "↩ inner return" },
+      { "aa", select "@parameter.outer", mode = { "x", "o" }, desc = "󰏪 outer arg" },
+      { "ia", select "@parameter.inner", mode = { "x", "o" }, desc = "󰏪 inner arg" },
+      { "iu", select "@loop.inner", mode = { "x", "o" }, desc = "󰛤 inner loop" },
+      { "au", select "@loop.outer", mode = { "x", "o" }, desc = "󰛤 outer loop" },
+      { "al", select "@call.outer", mode = { "x", "o" }, desc = "󰡱 outer call" },
+      { "il", select "@call.inner", mode = { "x", "o" }, desc = "󰡱 inner call" },
+      { "af", select "@function.outer", mode = { "x", "o" }, desc = " outer function" },
+      { "if", select "@function.inner", mode = { "x", "o" }, desc = " inner function" },
+      { "ao", select "@conditional.outer", mode = { "x", "o" }, desc = "󱕆 outer condition" },
+      { "io", select "@conditional.inner", mode = { "x", "o" }, desc = "󱕆 inner condition" },
+    },
   },
 }
