99156bd6b4e316618e2f1d628d8c55c42d438d69
Gustav Eikaas <gustav.eikaas@rotorsystems.no>
2026-02-04 20:04:13 +0100

    feat: vue add commentstring

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "vue",
+  callback = function()
+    vim.bo.commentstring = "<!-- %s -->"
+  end,
+})
+
 ---@type vim.lsp.Config
 return {
   init_options = {
